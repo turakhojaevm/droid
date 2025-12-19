4f2f67195ad2624f90015b99f43ec4c74cba2c9d
Makhkamkhoja <m.turakhojaev@eskhata.com>
2025-12-19 14:08:37 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let slides = ['mySlides', 'reviewSlides', 'resultSliders'];
+let slideIndex = 1;
+slides.forEach(className => {
+    showSlides(slideIndex, className);
+});
+
+// Next/previous controls
+function priceListSlider(n) {
+    showSlides(slideIndex += n, "mySlides");
+}
+function reviewSlider(n) {
+    showSlides(slideIndex += n, "reviewSlides");
+}
+function resultsSlider(n) {
+    showSlides(slideIndex += n, "resultSliders");
+}
+
+function showSlides(n, className) {
+    let i;
+    let slides = document.getElementsByClassName(className);
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
